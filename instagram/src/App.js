20dee6fd70194f6d9542927d39f3a29b4f2bbd1b
Moonkaman <nickbatmanglidj@gmail.com>
2019-01-14 15:09:30 -0700

    Import data and iterate over it returning jsx

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react';
 import HeaderNav from './Components/HeaderNav/HeaderNav'
 import PostsContainer from './Components/PostComponents/PostsContainer';
 
+import dummyData from './dummy-data';
+
 import './App.css';
-import SearchBar from './Components/HeaderNav/SearchBar';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      allPosts: dummyData
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <HeaderNav />
-        <PostsContainer />
+        <PostsContainer allPosts={this.state.allPosts} />
       </div>
     );
   }
--- instagram/src/Components/PostComponents/PostsContainer.js
+++ instagram/src/Components/PostComponents/PostsContainer.js
@@ -1,18 +1,27 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 import Post from './Post';
 
 const PostsContainer = props => {
   return (
     <div>
-      <h1>PostsContainer</h1>
-      <Post />
+      {props.allPosts.map(post => <Post key={post.timestamp + post.username} post={post} />)}
     </div>
   )
 }
 
 PostsContainer.propTypes = {
+  allPosts: PropTypes.arrayOf(PropTypes.shape({
+    username: PropTypes.string,
+    thumbnailUrl: PropTypes.string,
+    imageUrl: PropTypes.string,
+    likes: PropTypes.number,
+    timestamp: PropTypes.string,
+    comments: PropTypes.arrayOf(PropTypes.shape({
+      username: PropTypes.string,
+      text: PropTypes.string
+    }))
+  }))
+};
 
-}
-
-export default PostsContainer
+export default PostsContainer;
