e167555dfd799f96b16c4a26728511ed2d3e48ef
Moonkaman <nickbatmanglidj@gmail.com>
2019-01-17 13:48:27 -0700

    Refactor HeaderNav and SearchBar to use styled components

--- instagram/src/Components/HeaderNav/HeaderNav.js
+++ instagram/src/Components/HeaderNav/HeaderNav.js
@@ -1,30 +1,66 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
 import SearchBar from './SearchBar';
+import instaLogo from '../../imgs/instalogo.png';
 
 import '../../../node_modules/@fortawesome/fontawesome-free/css/all.css';
-import './HeaderNav.css';
+//import './HeaderNav.css';
 
-import instaLogo from '../../imgs/instalogo.png';
+const NavHeaderContainer = styled.div`
+  padding: 10px 10px;
+  border-bottom: 1px solid lightgray;
+  position: fixed;
+  width: 100vw;
+  background: white;
+`
+
+const NavHeaderFlex = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const InstaLogo = styled.i`
+  font-size: 30px;
+  position: relative;
+  border-right: 1px solid black;
+  padding-right: 18px;
+`
+
+const InstaCursiveLogo = styled.img`
+  height: 32px;
+  width: auto;
+  margin-left: 15px;
+  position: relative;
+  top: 8px;
+`
+
+const HeaderButtons = styled.i`
+  font-size: 22px;
+  color: gray;
+  margin: 0 15px;
+`
 
 const HeaderNav = props => {
   return (
-    <div className='header-nav-cont'>
-      <div className="header-nav-flex">
+    <NavHeaderContainer>
+      <NavHeaderFlex>
         <div>
-          <i className="fab fa-instagram insta-logo"></i>
-          <img className='insta-cursive' src={instaLogo} alt="" />
+          <InstaLogo className="fab fa-instagram insta-logo"></InstaLogo>
+          <InstaCursiveLogo className='insta-cursive' src={instaLogo} alt="" />
         </div>
         <div>
           <SearchBar searchInput={props.searchInput} handleSearchChange={props.handleSearchChange} handleSearch={props.handleSearch} />
         </div>
         <div>
-          <i className="far fa-compass"></i>
-          <i className="far fa-heart"></i>
-          <i className="far fa-user"></i>
+          <HeaderButtons className="far fa-compass"></HeaderButtons>
+          <HeaderButtons className="far fa-heart"></HeaderButtons>
+          <HeaderButtons className="far fa-user"></HeaderButtons>
         </div>
-      </div>
-    </div>
+      </NavHeaderFlex>
+    </NavHeaderContainer>
   )
 }
 
--- instagram/src/Components/HeaderNav/SearchBar.js
+++ instagram/src/Components/HeaderNav/SearchBar.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components';
+
+const HeaderSearchBar = styled.input`
+  background: #FAFAFA;
+  outline: none;
+  border: none;
+  border: 1px solid #DDDDDD;
+  text-align: center;
+  padding: 5px 0;
+  border-radius: 4px;
+  font-size: 14px;
+  font-weight: bold;
+  width: 225px;
+`
 
 const SearchBar = props => {
   return (
     <form name='Searchbar' onSubmit={props.handleSearch}>
-      <input 
+      <HeaderSearchBar 
       className='search-bar'
       type="text"
       placeholder='Search'
